c66bd3fc2994f02e6e1c68b2e18cca4266efe0fd
djmousefarm <djmousefarm@googlemail.com>
2016-06-19 19:11:07 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Write your main program here. Implementing your own classes will be useful.
+        Scanner reader = new Scanner(System.in);
+        Dictionary dict = new Dictionary();
+    }
+}
