904328933a0d79512fcdc6d376289c35ccd92254
djmousefarm <djmousefarm@googlemail.com>
2016-06-21 18:25:04 +0100

    Created by Coding Ground Client

---
+++ Car Registry/RegistrationPlate.java
@@ -0,0 +1,53 @@
+public class RegistrationPlate {
+    // ATTENTION: the object variable types are final, meaning that their value cannot be changed!
+    private final String regCode;
+    private final String country;
+
+    public RegistrationPlate(String regCode, String country) {
+       this.regCode = regCode;
+       this.country = country;
+    }
+
+    public String toString(){
+        return country+ " "+regCode;
+    }
+    
+    public String getRegCode() {
+        return this.regCode;
+    }
+    
+    public String getCountry() {
+        return this.country;
+    }
+    
+    @Override
+    public boolean equals(Object object) {
+        if (object == null) {
+            return false;
+        }
+
+        if (getClass() != object.getClass()) {
+            return false;
+        }
+
+        RegistrationPlate compared = (RegistrationPlate) object;
+
+        if (this.regCode != compared.getRegCode()) {
+            return false;
+        }
+
+        if (this.country == null || !this.country.equals(compared.getCountry())) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public int hashCode() {
+        if (this.regCode == null) {
+            return 7;
+        }
+
+        return this.regCode.hashCode() + this.country.hashCode();
+    }    
+}
