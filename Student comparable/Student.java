38d75569088eec36eaced6670f11886e6a2cb8d3
djmousefarm <djmousefarm@googlemail.com>
2016-06-29 10:53:05 +0100

    Created by Coding Ground Client

---
+++ Student comparable/Student.java
@@ -0,0 +1,23 @@
+public class Student implements Comparable<Student>{
+    private String name;
+    
+    public Student (String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    @Override
+    public int compareTo(Student student) {
+        if (this.name.compareToIgnoreCase(student.getName())==0) {
+            return 0;
+        } else if (this.name.compareToIgnoreCase(student.getName())>0) {
+            return 1;
+        } else {
+            return -1;
+        }    
+    }
+
+}
